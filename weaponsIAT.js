e04666c003e5a492a6111c3f460274ba143e214f
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-06-14 18:44:57 +0300

    Update weaponsIAT.js

--- weaponsIAT.js
+++ weaponsIAT.js
@@ -1,6 +1,6 @@
 define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat8.js'], function(APIConstructor, iatExtension){
 
-	var baseURL = 'https://elinorbengayev.github.io/weaponImages/';
+	var baseURL = 'https://baranan.github.io/minno-tasks/images/';
 	return iatExtension({
         category2 : {
             name : 'Black Americans', //Will appear in the data.
