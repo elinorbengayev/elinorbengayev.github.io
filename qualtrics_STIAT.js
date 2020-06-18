25c4dc58fb96d760fbccb4cba31563bc9f7d5061
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-06-18 13:08:18 +0300

    Update qualtrics_STIAT.js

--- qualtrics_STIAT.js
+++ qualtrics_STIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/stiat6.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://elinorbengayev.github.io/STIAT.js'], function(APIConstructor, stiatExtension){
   var API = new APIConstructor();
   
   
@@ -23,45 +23,45 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/sti
 
   		attribute1 : 
 			{
-				name : 'Unpleasant', //Attribute label
-				title : {
-					media : {word : 'Negative'}, //Name of the category presented in the task.
-					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
-					height : 7 //Used to position the "Or" in the combined block.
-				}, 
-				media : [ //Stimuli
-					{word: 'Bomb'},
-					{word: 'Abuse'},
-					{word: 'Sadness'},
-					{word: 'Pain'},
-					{word: 'Poison'},
-					{word: 'Grief'}
-				], 
-				//Can change color and size of the targets here.
-				css : {color:'#31b404','font-size':'2em'}
+			name : 'Unpleasant', //Attribute label
+			title : {
+				media : {word : 'Negative'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Bomb'},
+				{word: 'Abuse'},
+				{word: 'Sadness'},
+				{word: 'Pain'},
+				{word: 'Poison'},
+				{word: 'Grief'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'2em'}
 			},
-			attribute2 : 
+		attribute2 : 
 			{
-				name : 'Pleasant', //Attribute label
-				title : {
-					media : {word : 'Positive'}, //Name of the category presented in the task.
-					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
-					height : 7 //Used to position the "Or" in the combined block.
-				}, 
-				media : [ //Stimuli
-					{word: 'Paradise'},
-					{word: 'Pleasure'},
-					{word: 'Cheer'},
-					{word: 'Wonderful'},
-					{word: 'Splendid'},
-					{word: 'Love'}
-				], 
-				//Can change color and size of the targets here.
-				css : {color:'#31b404','font-size':'2em'}
+			name : 'Pleasant', //Attribute label
+			title : {
+				media : {word : 'Positive'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Paradise'},
+				{word: 'Pleasure'},
+				{word: 'Cheer'},
+				{word: 'Wonderful'},
+				{word: 'Splendid'},
+				{word: 'Love'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'2em'}
 			},
 
   //base_url : {//Where are your images at?
     //image : base_url
   } 
-  )
+  );
   });
