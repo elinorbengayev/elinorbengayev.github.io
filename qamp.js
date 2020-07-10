44af6db8644ae561f051494fa89bb2b8ce4dccff
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-07-10 18:34:11 +0300

    Update qamp.js

--- qamp.js
+++ qamp.js
@@ -281,7 +281,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
             // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
             serialize: function (name, logs) {
                 //var headers = ['block', 'trial', 'cond', 'type', 'cat',  'stim', 'resp', 'err', 'rt', 'd', 'fb', 'bOrd'];
-		var headers = ['block','trial','cond', 'cat',  'stim', 'resp', 'err', 'rt','fb'];
+		var headers = ['block','trial','cond', 'cat',  'stim', 'resp', 'err', 'rt'];
                 console.log(logs);
                 var myLogs = [];
                 var iLog;
@@ -324,7 +324,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
                         log.data.score, //'err'
                         log.latency, //'rt'
                         //'', //'d'
-                        '' //'fb'
+                        //'' //'fb'
                         //'' //'bOrd'
                         ]; });
 		    
@@ -353,7 +353,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
                             '', //'err'
                             '', //'rt'
                             //piCurrent.d, //'d'
-                            piCurrent.feedback //'fb'
+                            //piCurrent.feedback //'fb'
                             //block2Condition //'bOrd'
                         ]);
                 console.log(content);
@@ -383,11 +383,11 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
                     var quotableRgx = /(\n|,|")/;
                     if (quotableRgx.test(val)) return '"' + val.replace(/"/g, '""') + '"';
                     return val;
-                }
+                } 
             },
             // Set logs into an input (i.e. put them wherever you want)
             send: function(name, serialized){
-		console.log(serialized);
+		//console.log(serialized);
 		//console.log(name);
                 window.minnoJS.logger(serialized);
             }
