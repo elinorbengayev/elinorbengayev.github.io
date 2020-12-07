4ddd6afd8ced3fc61a7b7a38be9ceea9f9dcffb8
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-12-07 19:02:55 +0200

    Update newIAT.js

--- IAT_GUI/newIAT.js
+++ IAT_GUI/newIAT.js
@@ -1,3 +1,4 @@
+////new
 define(['pipAPI',"https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js"], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
     "category1": {
         "name": "Black people",
