46782bd332dffc2adc562fa29fc56f0f02e0fad7
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-12-21 10:57:30 +0200

    Update tst_biat.js

--- tst_biat.js
+++ tst_biat.js
@@ -3,7 +3,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
 
 	
 	return iatExtension({
-		isTouch: true,
+		isTouch: false,
 		practiceCategory1 : 
 			{
 				name : 'Mammals', //Will appear in the data.
