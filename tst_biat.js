f31c113e1445f3c317aa8fc0188b27cb6c47decb
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2020-12-21 12:29:12 +0200

    Update tst_biat.js

--- tst_biat.js
+++ tst_biat.js
@@ -57,7 +57,7 @@ define(['pipAPI', 'https://elinorbengayev.github.io/biat.js'], function(APIConst
 						height : 4, //Height (because we need to know where to put the next item in the title)
 						startStimulus : { 
 						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-						media : [{image : 'black1.jpg'},{image: 'black2.jpg'}],
+						media : {image: 'black2.jpg'},
 							
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 2
@@ -81,7 +81,7 @@ define(['pipAPI', 'https://elinorbengayev.github.io/biat.js'], function(APIConst
 						css : {color:'#31b404','font-size':'1.8em'}, 
 						height : 4,
 						startStimulus : {
-							media : [{image : 'yf4.jpg'},{image: 'yf1.jpg'}],
+							media : {image: 'yf1.jpg'},
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 2
 						}
