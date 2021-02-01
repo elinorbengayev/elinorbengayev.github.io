7ecb988e0ac47301d11fc97aec3662636430ecad
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-02-01 11:40:07 +0200

    Update biatTest.js

--- biatTest.js
+++ biatTest.js
@@ -1,99 +1,14 @@
-define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension)
-       {
-    var API = new APIConstructor(); return iatExtension({
-    "isTouch": false,
-    "practiceCategory1": {
-        "name": "Mammals",
-        "title": {
-            "media": {
-                "word": "Mammals"
-            },
-            "css": {
-                "color": "#31b404",
-                "font-size": "1.8em"
-            },
-            "height": 4,
-            "startStimulus": {
-                "media": {
-                    "word": "Dogs, Horses, Cows, Lions"
-                },
-                "css": {
-                    "color": "#31b404",
-                    "font-size": "1em"
-                },
-                "height": 2
-            }
-        },
-        "stimulusMedia": [
-            {
-                "word": "Dogs"
-            },
-            {
-                "word": "Horses"
-            },
-            {
-                "word": "Lions"
-            },
-            {
-                "word": "Cows"
-            }
-        ],
-        "stimulusCss": {
-            "color": "#31b404",
-            "font-size": "2em"
-        }
-    },
-    "practiceCategory2": {
-        "name": "Birds",
-        "title": {
-            "media": {
-                "word": "Birds"
-            },
-            "css": {
-                "color": "#31b404",
-                "font-size": "1.8em"
-            },
-            "height": 4,
-            "startStimulus": {
-                "media": {
-                    "word": "Pigeons, Swans, Crows, Ravens"
-                },
-                "css": {
-                    "color": "#31b404",
-                    "font-size": "1em"
-                },
-                "height": 2
-            }
-        },
-        "stimulusMedia": [
-            {
-                "word": "Pigeons"
-            },
-            {
-                "word": "Swans"
-            },
-            {
-                "word": "Crows"
-            },
-            {
-                "word": "Ravens"
-            }
-        ],
-        "stimulusCss": {
-            "color": "#31b404",
-            "font-size": "2em"
-        }
-    },
+define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
     "categories": [
         {
-            "name": "Black people",
+            "name": "Mammals",
             "title": {
                 "media": {
-                    "word": "Black people"
+                    "word": "Mammals"
                 },
                 "css": {
-                    "color": "#31b404",
-                    "font-size": "1.8em"
+                    "color": "#df2a2a",
+                    "font-size": "2em"
                 },
                 "height": 4,
                 "startStimulus": {
@@ -128,18 +43,18 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
                 }
             ],
             "stimulusCss": {
-                "color": "#31b404",
-                "font-size": "2em"
+                "color": "#baa08c",
+                "font-size": "4em"
             }
         },
         {
-            "name": "White people",
+            "name": "Dogs",
             "title": {
                 "media": {
-                    "word": "White people"
+                    "word": "Dogs"
                 },
                 "css": {
-                    "color": "#31b404",
+                    "color": "#5976cf",
                     "font-size": "1.8em"
                 },
                 "height": 4,
@@ -175,7 +90,7 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
                 }
             ],
             "stimulusCss": {
-                "color": "#31b404",
+                "color": "#be97d3",
                 "font-size": "2em"
             }
         }
@@ -264,8 +179,9 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
     },
     "base_url": "",
     "remindError": true,
-    "showStimuliWithInst": true,
-    "practiceBlock": true,
+    "showStimuliWithInst": false,
+    "isTouch": true,
+    "practiceBlock": false,
     "isQualtrics": true,
     "nMiniBlocks": 1,
     "nTrialsPerMiniBlock": 16,
@@ -274,11 +190,10 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
     "focalAttribute": "attribute1",
     "firstFocalAttribute": "random",
     "focalCategoryOrder": "random",
-    "leftKeyText": "\"E\" for all else",
-    "rightKeyText": "\"I\" if item belongs",
+    "rightKeyText": "Left for all else",
+    "leftKeyText": "Right if item belongs",
     "orKeyText": "or",
-    "remindErrorText": "<p align=\"center\" style=\"font-size:\"0.6em\"; font-family:arial\">If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<p/>",
-    "finalText": "Press space to continue to the next task",
-    "instTemplate": "<div><p align=\"center\" style=\"font-size:20px; font-family:arial\"><br/><font color=\"#000000\"><u>Part blockNum of nBlocks </u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\">Put a right finger on the <b>I</b> key for items that belong to the category <font color=\"#0000FF\">focalAtt</font>, and for items that belong to the category <font color=\"#31b404\">focalCat</font>.<br/>Put a left finger on the <b>E</b> key for items that do not belong to these categories.<br/><br/>If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<br/><br/><p align=\"center\">Press the <b>space bar</b> when you are ready to start.</font></p></div>"
-});
-   });
+    "remindErrorText": "<p align=\"center\" style=\"font-size:\"1.4em\"; font-family:arial\">If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Touch the other side to continue.<p/>",
+    "finalText": "Touch the bottom green area to continue to the next task",
+    "instTemplate": "<div><p align=\"center\" <br/><font color=\"#000000\"><u>Part blockNum of nBlocks </u><br/></p><p align=\"left\" style=\"margin-left:5px\"> Put a right finger on the <b>right</b> green area for items that belong to the category <font color=\"#0000FF\">focalAtt</font>, and for items that belong to the category <font color=\"#31b404\">focalCat</font>.<br/>Put a left finger on the <b>left</b> green area for items that do not belong to these categories.<br/>If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<br/><p align=\"center\">Touch the <b>lower </b> green area to start.</font></p></div>"
+});});
