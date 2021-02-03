9bae8956ec294be5842c4bad246544877eabd194
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-02-03 09:38:55 +0200

    Update exampleIAT.js

--- IAT_GUI/exampleIAT.js
+++ IAT_GUI/exampleIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat10.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://elinorbengayev.github.io/quiat10.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
