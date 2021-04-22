cf57bc2ac123cdb9835370731d640d2021f156dc
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-04-22 09:49:00 +0300

    Add files via upload

---
+++ IAT_GUI/STIAT.js
@@ -0,0 +1,142 @@
+define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/STIAT/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension) {var API = new APIConstructor(); return stiatExtension({
+    "category": {
+        "name": "BlacK",
+        "title": {
+            "media": {
+                "word": "BlacK"
+            },
+            "css": {
+                "color": "#292928",
+                "font-size": "6em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [
+            {
+                "word": "Tayron"
+            },
+            {
+                "word": "Malik"
+            },
+            {
+                "word": "Terrell"
+            },
+            {
+                "word": "Jazamin"
+            },
+            {
+                "word": "Tiara"
+            },
+            {
+                "word": "Shanice"
+            },
+            {
+                "word": "OGU"
+            }
+        ],
+        "css": {
+            "color": "#31b404",
+            "font-size": "2em"
+        }
+    },
+    "attribute1": {
+        "name": "Un",
+        "title": {
+            "media": {
+                "word": "Unpleasant"
+            },
+            "css": {
+                "color": "#69889b",
+                "font-size": "2em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [],
+        "css": {
+            "color": "#31b404",
+            "font-size": "2em"
+        }
+    },
+    "attribute2": {
+        "name": "Pleasant",
+        "title": {
+            "media": {
+                "word": "Pleasant"
+            },
+            "css": {
+                "color": "#31b404",
+                "font-size": "6em"
+            },
+            "height": 4
+        },
+        "stimulusMedia": [],
+        "css": {
+            "color": "#31b404",
+            "font-size": "2em"
+        }
+    },
+    "base_url": "",
+    "trialsByBlock": [
+        {
+            "instHTML": "",
+            "block": 1,
+            "miniBlocks": 1,
+            "singleAttTrials": 10,
+            "sharedAttTrials": 10,
+            "categoryTrials": 0
+        },
+        {
+            "instHTML": "",
+            "block": 2,
+            "miniBlocks": 2,
+            "singleAttTrials": 10,
+            "sharedAttTrials": 7,
+            "categoryTrials": 7
+        },
+        {
+            "instHTML": "",
+            "block": 3,
+            "miniBlocks": 2,
+            "singleAttTrials": 10,
+            "sharedAttTrials": 7,
+            "categoryTrials": 7
+        },
+        {
+            "instHTML": "",
+            "block": 4,
+            "miniBlocks": 2,
+            "singleAttTrials": 10,
+            "sharedAttTrials": 7,
+            "categoryTrials": 7
+        },
+        {
+            "instHTML": "",
+            "block": 5,
+            "miniBlocks": 2,
+            "singleAttTrials": 10,
+            "sharedAttTrials": 7,
+            "categoryTrials": 7
+        },
+        {
+            "instHTML": "",
+            "block": 6,
+            "miniBlocks": 3,
+            "singleAttTrials": 3,
+            "sharedAttTrials": 0,
+            "categoryTrials": 0
+        }
+    ],
+    "isTouch": true,
+    "blockOrder": "startLeft",
+    "switchSideBlock": 8,
+    "isQualtrics": true,
+    "leftKeyText": "",
+    "rightKeyText": "",
+    "orKeyText": "",
+    "remindErrorText": "",
+    "finalText": "",
+    "instTemplatePractice": "",
+    "instTemplateCategoryRight": "",
+    "instTemplateCategoryLeft": "",
+    "textOnError": ""
+})});
