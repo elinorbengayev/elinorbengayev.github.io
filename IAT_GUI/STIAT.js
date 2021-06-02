567712d2a7e131edf2c6519b375012eab125064d
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-06-02 09:09:17 +0300

    Update STIAT.js

--- IAT_GUI/STIAT.js
+++ IAT_GUI/STIAT.js
@@ -1,18 +1,18 @@
 define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension) {var API = new APIConstructor(); return stiatExtension({
     "category": {
-        "name": "Black people",
+        "name": "Black peopledwdaw",
         "title": {
             "media": {
-                "word": "Black people"
+                "word": "Black peoplesadsads"
             },
             "css": {
-                "color": "#262725",
+                "color": "#31b404",
                 "font-size": "2em"
             },
             "height": 4
         },
         "css": {
-            "color": "#df564e",
+            "color": "#31b404",
             "font-size": "2em"
         },
         "media": [
@@ -20,24 +20,27 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
                 "word": "Tayron"
             },
             {
-                "word": "Jazamin"
+                "word": "Malik"
+            },
+            {
+                "word": "Tiara"
             },
             {
                 "word": "Shanice"
             },
             {
-                "word": "חטע"
+                "word": "dasdas"
             }
         ]
     },
     "attribute1": {
-        "name": "Unpleasant",
+        "name": "Unpleasantdad",
         "title": {
             "media": {
-                "word": "Unpleasant"
+                "word": "Unpleasantdadas"
             },
             "css": {
-                "color": "#e87dc6",
+                "color": "#444bab",
                 "font-size": "2em"
             },
             "height": 4
@@ -64,14 +67,17 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
             },
             {
                 "word": "Grief"
+            },
+            {
+                "word": "dsadsa"
             }
         ]
     },
     "attribute2": {
-        "name": "Plea",
+        "name": "Pleasantdsad",
         "title": {
             "media": {
-                "word": "Plea"
+                "word": "Pleasantdsadsad"
             },
             "css": {
                 "color": "#31b404",
@@ -101,6 +107,9 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
             },
             {
                 "word": "Love"
+            },
+            {
+                "word": "dsadsa"
             }
         ]
     },
@@ -125,18 +134,26 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
         {
             "instHTML": "",
             "block": 3,
-            "miniBlocks": 2,
-            "singleAttTrials": 10,
-            "sharedAttTrials": 7,
-            "categoryTrials": 8
+            "miniBlocks": 3,
+            "singleAttTrials": 3,
+            "sharedAttTrials": 3,
+            "categoryTrials": 3
+        },
+        {
+            "instHTML": "hey",
+            "block": 4,
+            "miniBlocks": 3,
+            "singleAttTrials": 4,
+            "sharedAttTrials": 5,
+            "categoryTrials": 6
         }
     ],
-    "blockOrder": "startLeft",
+    "blockOrder": "startRight",
     "switchSideBlock": 8,
     "isQualtrics": true,
-    "leftKeyText": "Press \"E\" for",
-    "rightKeyText": "Press \"I\" for",
-    "orKeyText": "or",
+    "leftKeyText": "",
+    "rightKeyText": "fefes",
+    "orKeyText": "orfsdfsd",
     "remindErrorText": "<p style=\"font-size:0.6em;font-family:arial sans-serif; text-align:center;\">If you make a mistake, a red <font-color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<p/>",
     "finalText": "You have completed this task<br/><br/>Press SPACE to continue.",
     "instTemplatePractice": "<div><p align=\"center\" style=\"font-size:20px; font-family:arial\"><font color=\"#000000\"><u>Part blockNum of nBlocks</u><br/><br/></p><p style=\"font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial\">Put a left finger on the <b>E</b> key for items that belong to the category <font color=\"#31b404\">attribute1</font>.<br/>Put a right finger on the <b>I</b> key for items that belong to the category <font color=\"#31b404\">attribute2</font>.<br/>Items will appear one at a time.<br/><br/>If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<br/><br/><p align=\"center\">Press the <b>space bar</b> when you are ready to start.</font></p></div>",
