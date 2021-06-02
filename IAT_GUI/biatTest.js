6f4abacc413ff4096b6abef71b12cf623b8e17fa
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-06-02 09:09:41 +0300

    Update biatTest.js

--- IAT_GUI/biatTest.js
+++ IAT_GUI/biatTest.js
@@ -1,14 +1,93 @@
 define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
-    "categories": [
-        {
-            "name": "Mammals",
-            "title": {
+    "practiceCategory1": {
+        "name": "Mammals",
+        "title": {
+            "media": {
+                "word": "Mammals"
+            },
+            "css": {
+                "color": "#31b404",
+                "font-size": "1.8em"
+            },
+            "height": 4,
+            "startStimulus": {
                 "media": {
-                    "word": "Mammals"
+                    "word": "Dogs, Lions"
                 },
                 "css": {
-                    "color": "#df2a2a",
-                    "font-size": "2em"
+                    "color": "#31b404",
+                    "font-size": "1em"
+                },
+                "height": 2
+            }
+        },
+        "stimulusMedia": [
+            {
+                "word": "Dogs"
+            },
+            {
+                "word": "Lions"
+            },
+            {
+                "word": "Cows"
+            }
+        ],
+        "stimulusCss": {
+            "color": "#31b404",
+            "font-size": "2em"
+        }
+    },
+    "practiceCategory2": {
+        "name": "Birds!!!!",
+        "title": {
+            "media": {
+                "word": "Birds!!!!"
+            },
+            "css": {
+                "color": "#31b404",
+                "font-size": "1.8em"
+            },
+            "height": 4,
+            "startStimulus": {
+                "media": {
+                    "word": "Pigeons, Swans, Crows, Ravens"
+                },
+                "css": {
+                    "color": "#31b404",
+                    "font-size": "1em"
+                },
+                "height": 2
+            }
+        },
+        "stimulusMedia": [
+            {
+                "word": "Pigeons"
+            },
+            {
+                "word": "Swans"
+            },
+            {
+                "word": "Crows"
+            },
+            {
+                "word": "Ravens"
+            }
+        ],
+        "stimulusCss": {
+            "color": "#31b404",
+            "font-size": "2em"
+        }
+    },
+    "categories": [
+        {
+            "name": "Black peoplefwfew",
+            "title": {
+                "media": {
+                    "word": "Black peoplefewfewfw"
+                },
+                "css": {
+                    "color": "#31b404",
+                    "font-size": "1.8em"
                 },
                 "height": 4,
                 "startStimulus": {
@@ -43,24 +122,24 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
                 }
             ],
             "stimulusCss": {
-                "color": "#baa08c",
-                "font-size": "4em"
+                "color": "#31b404",
+                "font-size": "2em"
             }
         },
         {
-            "name": "Dogs",
+            "name": "White",
             "title": {
                 "media": {
-                    "word": "Dogs"
+                    "word": "White"
                 },
                 "css": {
-                    "color": "#5976cf",
+                    "color": "#f36012",
                     "font-size": "1.8em"
                 },
                 "height": 4,
                 "startStimulus": {
                     "media": {
-                        "word": "Jake, Connor, Bradley, Alison, Emma, Emily"
+                        "word": "Jake, Connor, Alison, Emily"
                     },
                     "css": {
                         "color": "#31b404",
@@ -71,35 +150,52 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
             },
             "stimulusMedia": [
                 {
-                    "word": "Jake"
-                },
-                {
-                    "word": "Connor"
-                },
-                {
-                    "word": "Bradley"
-                },
-                {
-                    "word": "Allison"
-                },
-                {
-                    "word": "Emma"
-                },
-                {
                     "word": "Emily"
                 }
             ],
             "stimulusCss": {
-                "color": "#be97d3",
+                "color": "#31b404",
                 "font-size": "2em"
+            }
+        },
+        {
+            "name": "rfrfewf",
+            "title": {
+                "media": {
+                    "image": "rwere"
+                },
+                "css": {
+                    "color": "#000000",
+                    "font-size": "0em"
+                },
+                "height": 4,
+                "startStimulus": {
+                    "media": {
+                        "word": "rwerwe"
+                    },
+                    "css": {
+                        "color": "#000000",
+                        "font-size": "0em"
+                    },
+                    "height": 2
+                }
+            },
+            "stimulusMedia": [
+                {
+                    "word": "rwer"
+                }
+            ],
+            "stimulusCss": {
+                "color": "#000000",
+                "font-size": "0em"
             }
         }
     ],
     "attribute1": {
-        "name": "Pleasant",
+        "name": "Pleasantefewf",
         "title": {
             "media": {
-                "word": "Pleasant"
+                "word": "Pleasantfewfwf"
             },
             "css": {
                 "color": "#0000FF",
@@ -117,30 +213,17 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
                 "height": 2
             }
         },
-        "stimulusMedia": [
-            {
-                "word": "Joy"
-            },
-            {
-                "word": "Love"
-            },
-            {
-                "word": "Happy"
-            },
-            {
-                "word": "Good"
-            }
-        ],
+        "stimulusMedia": [],
         "stimulusCss": {
             "color": "#0000FF",
             "font-size": "2em"
         }
     },
     "attribute2": {
-        "name": "Unpleasant",
+        "name": "Unpleasantdfewf",
         "title": {
             "media": {
-                "word": "Unpleasant"
+                "word": "Unpleasantfwef"
             },
             "css": {
                 "color": "#0000FF",
@@ -160,9 +243,6 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
         },
         "stimulusMedia": [
             {
-                "word": "Horrible"
-            },
-            {
                 "word": "Nasty"
             },
             {
@@ -178,22 +258,22 @@ define(['pipAPI' ,'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
         }
     },
     "base_url": "",
-    "remindError": true,
-    "showStimuliWithInst": false,
+    "remindError": false,
+    "showStimuliWithInst": true,
     "isTouch": true,
-    "practiceBlock": false,
+    "practiceBlock": true,
     "isQualtrics": true,
     "nMiniBlocks": 1,
     "nTrialsPerMiniBlock": 16,
     "nPracticeBlockTrials": 8,
     "nCategoryAttributeBlocks": 4,
-    "focalAttribute": "attribute1",
-    "firstFocalAttribute": "random",
-    "focalCategoryOrder": "random",
-    "rightKeyText": "Left for all else",
-    "leftKeyText": "Right if item belongs",
-    "orKeyText": "or",
-    "remindErrorText": "<p align=\"center\" style=\"font-size:\"1.4em\"; font-family:arial\">If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Touch the other side to continue.<p/>",
+    "focalAttribute": "attribute2",
+    "firstFocalAttribute": "attribute2",
+    "focalCategoryOrder": "bySequence",
+    "rightKeyText": "vsdvsdvsd",
+    "leftKeyText": "svdsvsdvs",
+    "orKeyText": "vdsvsvssvsds",
+    "remindErrorText": "<p style=\"font-size:1.4em; font-family:arial sans-serif; text-align:center;\">If you make a mistake, a red <font-color=\"#ff0000\"><b>X</b></font> will appear. Touch the other side to continue.<p/>",
     "finalText": "Touch the bottom green area to continue to the next task",
-    "instTemplate": "<div><p align=\"center\" <br/><font color=\"#000000\"><u>Part blockNum of nBlocks </u><br/></p><p align=\"left\" style=\"margin-left:5px\"> Put a right finger on the <b>right</b> green area for items that belong to the category <font color=\"#0000FF\">focalAtt</font>, and for items that belong to the category <font color=\"#31b404\">focalCat</font>.<br/>Put a left finger on the <b>left</b> green area for items that do not belong to these categories.<br/>If you make a mistake, a red <font color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<br/><p align=\"center\">Touch the <b>lower </b> green area to start.</font></p></div>"
+    "instTemplate": "<div><p style=\"text-align:center;\"\" <br/><font-color=\"#000000\"><u>Part blockNum of nBlocks </u><br/></p><p style=\"text-align:left;\" style=\"margin-left:5px\"> Put a right finger on the <b>right</b> green area for items that belong to the category <font-color=\"#0000FF\">focalAtt</font>, and for items that belong to the category <font-color=\"#31b404\">focalCat</font>.<br/>Put a left finger on the <b>left</b> green area for items that do not belong to these categories.<br/>If you make a mistake, a red <font-color=\"#ff0000\"><b>X</b></font> will appear. Press the other key to continue.<br/><p style=\"text-align:center;\">Touch the <b>lower </b> green area to start.</font></p></div>"
 });});
