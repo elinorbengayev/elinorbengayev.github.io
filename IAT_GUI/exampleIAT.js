dbc20d70534983f431755d961edd55967e6c1fff
elinorbengayev <66786574+elinorbengayev@users.noreply.github.com>
2021-06-30 08:13:09 +0300

    Update exampleIAT.js

--- IAT_GUI/exampleIAT.js
+++ IAT_GUI/exampleIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI' ,'https://elinorbengayev.github.io/quiat10.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
+define(['pipAPI' ,'https://elinorbengayev.github.io/quiat11.js'], function(APIConstructor, iatExtension) {var API = new APIConstructor(); return iatExtension({
     "category1": {
         "name": "Black people!!!",
         "title": {
